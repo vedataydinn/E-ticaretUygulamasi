df2ab87640a162e0992fc0dff6e6ea593cf299a4
Vedat Aydın <vedataydin6523@gmail.com>
2024-04-05 18:12:41 +0300

    Register yeni kayıt ekranı oluşturuldu

---
+++ app/src/main/java/com/vedat/e_commerce/activities/loginRegisterActivity.kt
@@ -0,0 +1,14 @@
+package com.vedat.e_commerce.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.vedat.e_commerce.R
+
+class loginRegisterActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+
+    }
+
+}
