8c87d43d4e667ecb24b2cb849ef99b5adf683280
Vedat Aydın <vedataydin6523@gmail.com>
2024-04-26 11:23:26 +0300

    "mail kontrolü yapıldı"

--- app/src/main/java/com/vedat/e_commerce/util/ValidationCheck.kt
+++ app/src/main/java/com/vedat/e_commerce/util/ValidationCheck.kt
@@ -1,2 +1,20 @@
 package com.vedat.e_commerce.util
 
+import android.util.Patterns
+
+fun validateEmail(email:String):RegisterValidation{
+    if (email.isEmpty())
+        return RegisterValidation.Failed("email bos Gecilemez")
+    if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        return RegisterValidation.Failed("yanlış email ,sıfırla")
+
+    return RegisterValidation.Success
+}
+
+fun validationPassword(password:String):RegisterValidation{
+    if(password.isEmpty())
+        return RegisterValidation.Failed("Sifre bos olmamalı")
+    if(password.length<6)
+        return RegisterValidation.Failed("sifre 6 haneli olmalı")
+    return RegisterValidation.Success
+}
